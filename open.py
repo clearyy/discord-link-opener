c9af4c76a7483d94230214a298e53db9dda8b80a
Cleary <32603870+clearyy@users.noreply.github.com>
2019-11-10 19:33:16 -0500

    Fix field error and add improve on_ready check

--- open.py
+++ open.py
@@ -22,6 +22,9 @@ channels = []
 #enter token of discord account that has access to watch specified channels
 token = ''
 
+global start_count
+start_count = 0
+
 #check for keywords and blacklisted words in message urls and open browser if conditions are met
 async def check_urls(urls):
     for url in urls:
@@ -32,16 +35,18 @@ async def check_urls(urls):
 
 @client.event
 async def on_ready():
-    client.start_count = 0
+    global start_count
     # only print this info on the first time the client launches. this is due to d.py calling on_ready() after the bot regains connection
-    if client.start_count == 0:
+    if start_count == 0 and len(keywords) != 1 and keywords[0] != '':
         print('{} is ready to cop some restocks.\n'.format(str(client.user)))
         print('Watching for keywords {}.\n'.format(', '.join(keywords)))
         if len(blacklist) > 0:
-            print('Ignoring keywords {}'.format(', '.join(blacklist)))
+            print('Ignoring keywords {}\n'.format(', '.join(blacklist)))
         else:
             print('No keywords currently blacklisted.\n')
-        client.start_count += 1
+        start_count += 1
+    else:
+        print('You have not provided any keywords to monitor. Restart with keywords added to the list.')
 
 @client.event
 async def on_message(message):
@@ -49,9 +54,13 @@ async def on_message(message):
         if message.embeds:
             for embed in message.embeds:
                 toembed = embed.to_dict()
-                for field in toembed['fields']:
-                    urls = re.findall("(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+",str(field))
-                    await check_urls(urls)
+                #lazy fix
+                try:
+                    for field in toembed['fields']:
+                        urls = re.findall("(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+",str(field))
+                        await check_urls(urls)
+                except:
+                    pass
         else:
             urls = re.findall("(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+",message.content)
             await check_urls(urls)
